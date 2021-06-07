8c2f9914a858e217305619b1db2dbc2b5ae55ce1
Mikhail Prozorov <mikepro@alfamail.ru>
2021-06-07 15:31:50 +0300

    Lesson 1 initial

--- adpy-21-hw01/main.py
+++ adpy-21-hw01/main.py
@@ -1,10 +1,10 @@
 from application.salary import calculate_salary
 from application.db.people import get_employees
 from datetime import datetime
-# Press the green button in the gutter to run the script.
+
+
 if __name__ == '__main__':
     calculate_salary()
     get_employees()
     print(datetime.now().date())
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
