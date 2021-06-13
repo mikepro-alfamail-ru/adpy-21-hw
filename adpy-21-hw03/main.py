1ac9498ef0ad4426047d2543dd4b278f8a591eb4
Mikhail Prozorov <mikepro@alfamail.ru>
2021-06-13 18:42:10 +0300

    Homework3 update

--- adpy-21-hw03/main.py
+++ adpy-21-hw03/main.py
@@ -6,6 +6,7 @@ from tqdm import tqdm
 KEYWORDS = {'дизайн', 'фото', 'web', 'python'}
 BASEURL = 'https://habr.com/ru/all/'
 
+
 # Ваш код
 def get_url_text(url):
     response = requests.get(url)
@@ -21,23 +22,23 @@ def main():
     if text:
         soup = bs(text, features="html.parser")
         articles = soup.find_all('article', class_='post_preview')
-    for article in tqdm(articles):
-        post_time = article.find('span', class_='post__time').text
-        post_title = article.find('a', class_='post__title_link')
-        post_title_text = post_title.text
-        post_title_link = post_title['href']
-        post_text = article.find('div', class_='post__text').text
-        post_text_words = {word for word in post_text.split(' ')}
-        post_text_full = get_url_text(post_title_link)
-        post_fulltext_words = {}
-        if post_text_full:
-            soup_post = bs(post_text_full, features="html.parser")
-            post_fulltext = soup_post.find('div', class_='post__body_full').text
-            post_fulltext_words = {word for word in post_fulltext.split(' ')}
-        if (KEYWORDS & post_text_words) or (KEYWORDS & post_fulltext_words):
-            article_list.append(f'{post_time} - {post_title_text} - {post_title_link}')
+        for article in tqdm(articles):
+            post_time = article.find('span', class_='post__time').text
+            post_title = article.find('a', class_='post__title_link')
+            post_title_text = post_title.text
+            post_title_link = post_title['href']
+            post_text = article.find('div', class_='post__text').text
+            post_text_words = {word for word in post_text.split(' ')}
+            post_text_full = get_url_text(post_title_link)
+            post_fulltext_words = {}
+            if post_text_full:
+                soup_post = bs(post_text_full, features="html.parser")
+                post_fulltext = soup_post.find('div', class_='post__body_full').text
+                post_fulltext_words = {word for word in post_fulltext.split(' ')}
+            if (KEYWORDS & post_text_words) or (KEYWORDS & post_fulltext_words):
+                article_list.append(f'{post_time} - {post_title_text} - {post_title_link}')
+        print(*article_list, sep='\n')
 
-    print(*article_list, sep='\n')
 
 if __name__ == '__main__':
     main()
