fcdb3c832b2d67b9d3ef66103c50f115fc651129
Mikhail Prozorov <mikepro@alfamail.ru>
2021-06-14 23:29:39 +0300

    Homework 5 update

--- adpy-21-hw05/logger.py
+++ adpy-21-hw05/logger.py
@@ -5,6 +5,30 @@ LOGFILE = 'log.txt'
 
 
 def datelogger(function):
+    def _func(*args, **kwargs):
+        print(datetime.now())
+        result = function(*args, **kwargs)
+        return result
+    return _func
+
+
+def loggertofile(logfile):
+    def logger(function):
+        def _func(*args, **kwargs):
+            nonlocal logfile
+            now = datetime.now()
+            with open(logfile, 'a', encoding='utf-8') as log:
+                log.write(f'Вызов функции {function.__name__}, время - {now}\n')
+            result = function(*args, **kwargs)
+            return result
+        return _func
+    return logger
+
+
+'''
+Как ни странно, но работает еще и в таком виде:
+
+def datelogger(function):
     print(datetime.now())
     return function
 
@@ -17,6 +41,7 @@ def loggertofile(logfile):
             log.write(f'Вызов функции {function.__name__}, время - {now}\n')
         return function
     return logger
+'''
 
 
 # @datelogger           # Задание 1
@@ -28,5 +53,5 @@ def md5gen(file):
 
 
 if __name__ == '__main__':
-    for md5 in md5gen('main.py'):
+    for md5 in md5gen('main2.py'):
         print(md5)
