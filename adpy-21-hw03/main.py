8f884481aa5bc6897ee6dff0d4e9d5da03c37bee
Mikhail Prozorov <mikepro@alfamail.ru>
2021-06-14 11:52:17 +0300

    Homework3 update: add more pages to scrap

--- adpy-21-hw03/main.py
+++ adpy-21-hw03/main.py
@@ -16,34 +16,42 @@ def get_url_text(url):
         return False
 
 
-def main(advaced_task=False):
-    text = get_url_text(BASEURL)
+def main(url=BASEURL, advaced_task=False):
+    text = get_url_text(url)
     article_list = []
     if text:
         soup = bs(text, features="html.parser")
         articles = soup.find_all('article', class_='post_preview')
         for article in tqdm(articles):
-            post_time = article.find('span', class_='post__time').text
-            post_title = article.find('a', class_='post__title_link')
-            post_title_text = post_title.text
-            post_title_link = post_title['href']
+            article_time = article.find('span', class_='post__time').text
+            article_title = article.find('a', class_='post__title_link')
+            article_title_text = article_title.text
+            article_title_link = article_title['href']
             if advaced_task:
                 # Далее код от дополнительного задания
                 # отсюда
-                post_text_full = get_url_text(post_title_link)
-                post_words = {}
-                if post_text_full:
-                    soup_post = bs(post_text_full, features="html.parser")
-                    post_fulltext = soup_post.find('div', class_='post__body_full').text
-                    post_words = {word for word in post_fulltext.split(' ')}
+                article_text_full = get_url_text(article_title_link)
+                article_words = {}
+                if article_text_full:
+                    article_post = bs(article_text_full, features="html.parser")
+                    article_fulltext = article_post.find('div', class_='post__body_full').text
+                    article_words = {word for word in article_fulltext.split(' ')}
                 # досюда
             else:
-                post_text = article.find('div', class_='post__text').text
-                post_words = {word for word in post_text.split(' ')}
-            if KEYWORDS & post_words:
-                article_list.append(f'{post_time} - {post_title_text} - {post_title_link}')
-        print(*article_list, sep='\n')
+                article_text = article.find('div', class_='post__text').text
+                article_words = {word for word in article_text.split(' ')}
+            if KEYWORDS & article_words:
+                article_list.append(f'{article_time} - {article_title_text} - {article_title_link}')
+        return article_list
 
 
 if __name__ == '__main__':
-    main(True)
+    output_list = []
+    pages = int(input('Введите количество страниц: '))
+    for page in range(pages):
+        print(f'Обрабатываем страницу {page + 1} из {pages}')
+        if page == 0:
+            output_list += main(advaced_task=True)
+        else:
+            output_list += main(url=f'{BASEURL}page{page+1}/', advaced_task=True)
+    print(*output_list, sep='\n')
